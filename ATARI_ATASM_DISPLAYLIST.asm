7c092c118c211c82d88c8bf63ad3a87f9e1beaf7
Ken Jennings <kj.sumter.42@gmail.com>
2017-05-28 20:19:04 -0400

    Create ATARI_ATASM_DISPLAYLIST.asm
    Hello, Whirled! accomplished without direct code.   It overwrites the Display list LMS address operand to show the text on screen.

---
+++ ATARI_ATASM_DISPLAYLIST.asm
@@ -0,0 +1,61 @@
+; --------------------------------------------------------------------
+; 6502 assembly on Atari.
+; Built with eclipse/wudsn/atasm.
+
+; "Hello, Whirled!" displayed by changing the graphics mode 0
+; display list's initial memory scan address to point to 
+; the buffer in the program.  
+;
+; There is also callous disregard for whatever may follow the 
+; program in memory.  The screen will also show whatever junk 
+; happens to follow in memory.  If that is nonzero bytes, then 
+; other characters will appear on screen.  (On a cold boot 
+; memory will likely be 0. But, if any program is run before 
+; this program, then there may be visible data.)
+;
+; There is no actual code doing work. This is accomplished by 
+; using the Atari's structured executable load file format to 
+; load the address of the Hello, Whirled! string directly to 
+; the LMS instruction's address operand.
+; 
+; $BC20 is the starting address of the display list when 
+; there is 48K or more of memory and NO CARTRIDGE loaded.
+; The mode instruction is at the base display list address
+; plus 3 bytes.  The LMS address operand is the word that
+; follows at display list base plus 4.
+;
+DISPLAY_LIST=$BC20
+
+LOMEM_DOS_DUP = $3308 ; First usable memory after DOS and DUP 
+
+DOS_RUN_ADDR =  $02e0 ; Execute at address stored here when file loading completes.
+
+; --------------------------------------------------------------------
+
+	*=LOMEM_DOS_DUP ; Start program at first usable memory after DOS and DUP 
+
+PRG_START
+
+Do_While_More_Electricity         ; Infinite loop, otherwise the
+	jmp Do_While_More_Electricity ; program returns to DOS immediately.
+; Yup, that's all the "program" there is. 3 bytes of JMP
+
+HELLO_WHIRLED
+	.sbyte "Hello, Whirled!"      ; .sbyte is internal Atari format
+
+; Therefore: Set program "location" to the display list LMS address operand. 
+	*=[DISPLAY_LIST+4] 
+ 	.word HELLO_WHIRLED ; And store the address of the string.
+; and that's it.  Hello, Whirled! with no code.
+
+
+; --------------------------------------------------------------------
+; Store the program start location in the Atari DOS RUN Address.
+; When DOS is done loading the executable it will automatically
+; jump to the address placed here in DOS_RUN_ADDR.
+
+	*=DOS_RUN_ADDR
+	.word PRG_START
+
+; --------------------------------------------------------------------
+	.end ; finito
